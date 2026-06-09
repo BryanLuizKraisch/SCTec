a686683b1f926a329ed31ad6754859b7a2f39334
BryanLuizKraisch <bryan@atitude3d.com>
2026-06-09 17:51:24 -0300

    primeiro commit

---
+++ CSS/aula20/script.js
@@ -0,0 +1,17 @@
+const turma1 = ["João", "Maria", "Pedro"];
+const turma2 = ["Carla", "Rafael"];
+
+const Aluno1Turma1 = "joão";
+const Aluno2Turma1 = "maria";
+const Aluno3Turma1 = "pedro";
+const Aluno1Turma2 = "carla";
+const Aluno2Turma2 = "rafael";
+
+console.log(
+  "alunos 1 da turma 1: " +
+    Aluno1Turma1 +
+    ", aluno 2 da turma 1: " +
+    Aluno2Turma1 +
+    ", aluno 3 da turma 1: " +
+    Aluno3Turma1,
+);
